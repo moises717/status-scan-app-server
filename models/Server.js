dcf49f73b757dd531a67e5afd1c2c26ebf38af03
Moises Barillas <moisesrocha717@gmail.com>
2021-01-04 21:56:45 -0600

    cors

--- models/Server.js
+++ models/Server.js
@@ -3,12 +3,13 @@ const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
 const Sockets = require("./Sockets");
-
+const cors = require("cors");
 class Server {
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT;
 
+		this.app.use(cors());
 		//Http server
 		this.server = http.createServer(this.app);
 
